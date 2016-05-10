2c943d90a47d18f92bd8bd0e570bd87e8a95ef3a
Jared Moore <swiftfoottim@gmail.com>
2016-05-10 16:21:14 -0400

    Added a new package that allows for launching multiple nodes that perform an adding service.  Call: roslaunch adding_service adding_service.launch  Then call python server.py in the test folder of the package.

---
+++ src/adding_service/src/adder_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# consumer
+ 
+import json
+import rospy
+import zmq
+ 
+context = zmq.Context()
+receiver = context.socket(zmq.PULL)
+receiver.connect('tcp://127.0.0.1:5000')
+ 
+sender = context.socket(zmq.PUSH)
+sender.connect('tcp://127.0.0.1:5010')
+ 
+while True:
+    data = json.loads(receiver.recv())
+    msg = json.dumps({'a':data['a'],'b':data['b'],'sum':data['a']+data['b'], 'ns':rospy.get_namespace()})
+    sender.send(msg)
+    print (rospy.get_namespace(), data['a']+data['b'])
