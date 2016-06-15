cdddb06101746926142de5ca56dd97cafc24dee2
Jared Moore <JMoore@Jareds-MacBook-Pro.local>
2016-06-15 13:59:32 -0400

    Basic GA implemented.

--- src/adding_service/test/ga_server.py
+++ src/adding_service/test/ga_server.py
@@ -87,7 +87,7 @@ class GA(object):
 
     def __init__(self):
         # Initialize the genomes with an id and randomly generated list of 20 floats under 1.0
-        self.genomes = [{'id':i,'genome':[random.random() for j in range(20)], 'fitness':-1.0} for i in range(10)]
+        self.genomes = [{'id':i,'genome':[random.random() for j in range(20)], 'fitness':-1.0} for i in range(100)]
         self.ga_communicator = GACommunicator()
 
     def calculate_fitnesses(self):
