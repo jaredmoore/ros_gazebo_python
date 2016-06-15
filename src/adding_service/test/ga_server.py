24e82f153a99f9cda0856d27b119b9dab6cdc708
Jared Moore <JMoore@Jareds-MacBook-Pro.local>
2016-06-15 10:50:08 -0400

    Modified to begin making it a class-based compuation.

--- src/adding_service/test/ga_server.py
+++ src/adding_service/test/ga_server.py
@@ -7,11 +7,12 @@ import random
  
 import threading
  
-class senderThread(threading.Thread):
-    def __init__(self, threadID, socket):
+class SenderThread(threading.Thread):
+    def __init__(self, threadID, socket, genomes):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.socket = socket
+        self.data = genomes
  
     def run(self):
         print("\t\t\t\tStarting "+str(self.threadID))
@@ -25,43 +26,72 @@ class senderThread(threading.Thread):
             socket: socket to send the data out on.
                 - Persistant throughout execution for now.
         """
-        for i in range(10):
-            msg = json.dumps({'a':random.random(),'b':random.random()*10.0})
+        for i in self.data:
+            msg = json.dumps(i)
             print(msg)
             socket.send(msg)
  
-# Setup the socket to send data out on.
-context = zmq.Context()
-socket = context.socket(zmq.PUSH)
-#socket.setsockopt(zmq.LINGER, 0)    # discard unsent messages on close
-socket.bind('tcp://127.0.0.1:5000')
+class GACommunicator(object):
+    """ Class to handle setting up the sockets and sending/receiving genome data. """
+
+    def __init__(self):
+        """ Initialize the socket for data. """
+
+        # Setup the socket to send data out on.
+        context = zmq.Context()
+        self.socket = context.socket(zmq.PUSH)
+        #socket.setsockopt(zmq.LINGER, 0)    # discard unsent messages on close
+        self.socket.bind('tcp://127.0.0.1:5000')
  
-# Setup the socket to read the responses on.
-receiver = context.socket(zmq.PULL)
-receiver.bind('tcp://127.0.0.1:5010')
+        # Setup the socket to read the responses on.
+        self.receiver = context.socket(zmq.PULL)
+        self.receiver.bind('tcp://127.0.0.1:5010')
+
+    def __del__(self):
+        """ Close the sockets. """
+        print("Closing Socket")
+        socket.close()
+        receiver.close()
+
+    def send_genomes(self,genomes):
+        """ Send the genomes through the sender thread to workers. 
+
+        Args:
+            genomes: list of genomes to send out.
+
+        Returns:
+            list of results containing the genome id and fitness
+        """
+
+        return_data = []
+
+        # Start a thread to send the data.
+        sendThread = SenderThread(1, self.socket, genomes)
+
+        sendThread.start()
  
+        # Read the responses on the receiver socket.
+        i = len(genomes)
+        while i > 0:
+            data = json.loads(self.receiver.recv())
+            #return_data.append({'id':data['id'], 'fitness':data['fitness']})
+            return_data.append('sum':data['sum'],'true_sum':data['a']+data['b'])
+            i -= 1
+         
+        # Wait for the send thread to complete.
+        sendThread.join()
+
+        return return_data
+
+ga_communicator = GACommunicator()
+
+genomes = [{'a':random.random(),'b':random.random()*10.0} for i in range(10)]
+ 
+# TODO: Auto-detect when workers are ready?
+# TODO: Change messages so they persist waiting for workers?
 print("Press Enter when the workers are ready: ")
 _ = raw_input()
 print("Sending tasks to workers")
- 
-# Start a thread to send the data.
-sendThread = senderThread(1, socket)
-sendThread.start()
- 
-# Read the responses on the receiver socket.
-i = 10
-while i > 0:
-    data = json.loads(receiver.recv())
-    print(data['sum'],data['a']+data['b'])
-    i -= 1
- 
-# Wait for the send thread to complete.
-sendThread.join()
- 
-# while threading.activeCount() > 0:
-#     #print(threading.activeCount())
-#     pass
- 
-print("Closing Socket")
-socket.close()
-receiver.close()
+
+
+
