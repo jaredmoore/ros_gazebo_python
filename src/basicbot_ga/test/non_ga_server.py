b56a7a9d1a1415809d21cd8fe3b7d5bdebac6471
Jared Moore <JMoore@Jareds-MacBook-Pro.local>
2016-07-18 08:32:45 -0400

    Added basic test without ga for basicbot launching.

---
+++ src/basicbot_ga/test/non_ga_server.py
@@ -0,0 +1,71 @@
+"""
+    Simple non-ga server for the basicbot.
+"""
+ 
+import json
+import zmq
+ 
+import random
+ 
+import threading
+ 
+class senderThread(threading.Thread):
+    def __init__(self, threadID, socket):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.socket = socket
+ 
+    def run(self):
+        print("\t\t\t\tStarting "+str(self.threadID))
+        self.send_data()
+        print("\t\t\t\tExiting "+str(self.threadID))
+ 
+    def send_data(self):
+        """ Send data to worker processes. 
+ 
+        Args:
+            socket: socket to send the data out on.
+                - Persistant throughout execution for now.
+        """
+        for i in range(10):
+            ind = {'id':i,'genome':[
+                    random.random()*10.0, # center_spin_thresh
+                    9.0 + random.random() * 1.0, # center_drive_thresh
+                    random.random() * 10.0, # center_stop_thresh
+                    random.random() * 10.0 # stopping_thresh
+                ], 'fitness':-1.0}
+            msg = json.dumps(ind)
+            print(msg)
+            socket.send(msg)
+ 
+# Setup the socket to send data out on.
+context = zmq.Context()
+socket = context.socket(zmq.PUSH)
+#socket.setsockopt(zmq.LINGER, 0)    # discard unsent messages on close
+socket.bind('tcp://127.0.0.1:5000')
+ 
+# Setup the socket to read the responses on.
+receiver = context.socket(zmq.PULL)
+receiver.bind('tcp://127.0.0.1:5010')
+ 
+print("Press Enter when the workers are ready: ")
+_ = raw_input()
+print("Sending tasks to workers")
+ 
+# Start a thread to send the data.
+sendThread = senderThread(1, socket)
+sendThread.start()
+ 
+# Read the responses on the receiver socket.
+i = 10
+while i > 0:
+    data = json.loads(receiver.recv())
+    print(data['fitness'],data['id'])
+    i -= 1
+ 
+# Wait for the send thread to complete.
+sendThread.join()
+ 
+print("Closing Socket")
+socket.close()
+receiver.close()
