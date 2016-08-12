c2eaacba757bf17303536d93bfa466b9c0e8cb09
Jared Moore <swiftfoottim@gmail.com>
2016-08-12 14:24:40 -0400

    Fixed error.

--- src/basicbot_ga/test/non_ga_server.py
+++ src/basicbot_ga/test/non_ga_server.py
@@ -30,13 +30,14 @@ class senderThread(threading.Thread):
             socket: socket to send the data out on.
                 - Persistant throughout execution for now.
         """
-        ind = {'id':i,'genome':[
+        ind = {'id':0,'genome':[
                     float("{0:.6f}".format(random.random()*10.0)), # center_spin_thresh
                     float("{0:.6f}".format(9.0 + random.random() * 1.0)), # center_drive_thresh
                     float("{0:.6f}".format(random.random()*10.0)), # center_stop_thresh
                     float("{0:.6f}".format(random.random()*10.0)) # stopping_thresh
                 ], 'fitness':-1.0}
         for i in range(self.num_genomes):
+            ind['id'] = i
             # ind = {'id':i,'genome':[
             #         float("{0:.6f}".format(random.random()*10.0)), # center_spin_thresh
             #         float("{0:.6f}".format(9.0 + random.random() * 1.0)), # center_drive_thresh
