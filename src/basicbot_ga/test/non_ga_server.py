7d9dff5c454d3d86dfb15099a93f02c3624a1c01
Jared Moore <swiftfoottim@gmail.com>
2017-04-09 19:23:27 -0400

    Changed test size to 20 individuals.

--- src/basicbot_ga/test/non_ga_server.py
+++ src/basicbot_ga/test/non_ga_server.py
@@ -63,7 +63,7 @@ _ = raw_input()
 print("Sending tasks to workers")
  
 # How many genomes to test sending
-test_genome_num = 150
+test_genome_num = 20
 
 # Start a thread to send the data.
 sendThread = senderThread(1, socket, num_genomes=test_genome_num)
