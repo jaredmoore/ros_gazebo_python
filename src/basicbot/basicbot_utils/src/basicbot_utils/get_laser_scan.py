Fixed a possible error in precision in the lsCallback.

src/basicbot/basicbot_utils/src/basicbot_utils/get_laser_scan.py
@@ -23,7 +23,7 @@ class GetLaserScanner(object):
         self.msg = msg
 
         # Correct for infinite values by replacing them with max range.
-        self.formatted_msg = {'time':str(msg.header.stamp.secs)+"."+str(msg.header.stamp.nsecs), 'sum_ranges':sum([float("{0:.6f}".format(i)) if i != np.inf else msg.range_max for i in msg.ranges]), 'ranges':[float("{0:.6f}".format(i)) if i != np.inf else msg.range_max for i in msg.ranges]}
+        self.formatted_msg = {'time':str(msg.header.stamp.secs)+"."+str(msg.header.stamp.nsecs), 'sum_ranges':sum([float("{0:.6f}".format(i)) if i != np.inf else float("{0:.6f}".format(msg.range_max) for i in msg.ranges]), 'ranges':[float("{0:.6f}".format(i)) if i != np.inf else float("{0:.6f}".format(msg.range_max) for i in msg.ranges]}
 
     def getScanState(self):
         """ Get the current scan information. """
@@ -47,8 +47,8 @@ class GetLaserScanner(object):
             partitions[2] = partitions[1] + partitions[2]
 
             # Get right, center, and left averages.
-            partitioned_vision['right'] =   float("{0:.6f}".format(sum(self.formatted_msg['ranges'][0:partitions[0]])/len(self.formatted_msg['ranges'][0:partitions[0]])))
-            partitioned_vision['center'] =  float("{0:.6f}".format(sum(self.formatted_msg['ranges'][partitions[0]:partitions[1]])/len(self.formatted_msg['ranges'][partitions[0]:partitions[1]])))
-            partitioned_vision['left'] =    float("{0:.6f}".format(sum(self.formatted_msg['ranges'][partitions[1]:partitions[2]])/len(self.formatted_msg['ranges'][partitions[1]:partitions[2]])))
+            partitioned_vision['right'] =   sum(self.formatted_msg['ranges'][0:partitions[0]])/len(self.formatted_msg['ranges'][0:partitions[0]])
+            partitioned_vision['center'] =  sum(self.formatted_msg['ranges'][partitions[0]:partitions[1]])/len(self.formatted_msg['ranges'][partitions[0]:partitions[1]])
+            partitioned_vision['left'] =    sum(self.formatted_msg['ranges'][partitions[1]:partitions[2]])/len(self.formatted_msg['ranges'][partitions[1]:partitions[2]])
 
         return partitioned_vision