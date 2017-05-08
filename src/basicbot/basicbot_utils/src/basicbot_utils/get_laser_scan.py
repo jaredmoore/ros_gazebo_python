8d8a0c0dc117997d82a1409def88548f5c8d1110
Jared Moore <swiftfoottim@gmail.com>
2017-05-07 20:08:26 -0400

    Changed formatting of getLeftCenterRightScanState to a precision of 6 decimal places.

--- src/basicbot/basicbot_utils/src/basicbot_utils/get_laser_scan.py
+++ src/basicbot/basicbot_utils/src/basicbot_utils/get_laser_scan.py
@@ -47,8 +47,8 @@ class GetLaserScanner(object):
             partitions[2] = partitions[1] + partitions[2]
 
             # Get right, center, and left averages.
-            partitioned_vision['right'] = float("{0:.6f}".format(sum(self.formatted_msg['ranges'][0:partitions[0]])/len(self.formatted_msg['ranges'][0:partitions[0]]))
-            partitioned_vision['center'] = float("{0:.6f}".format(sum(self.formatted_msg['ranges'][partitions[0]:partitions[1]])/len(self.formatted_msg['ranges'][partitions[0]:partitions[1]]))
-            partitioned_vision['left'] = float("{0:.6f}".format(sum(self.formatted_msg['ranges'][partitions[1]:partitions[2]])/len(self.formatted_msg['ranges'][partitions[1]:partitions[2]]))
+            partitioned_vision['right'] =   float("{0:.6f}".format(str(sum(self.formatted_msg['ranges'][0:partitions[0]])/len(self.formatted_msg['ranges'][0:partitions[0]]))))
+            partitioned_vision['center'] =  float("{0:.6f}".format(str(sum(self.formatted_msg['ranges'][partitions[0]:partitions[1]])/len(self.formatted_msg['ranges'][partitions[0]:partitions[1]]))))
+            partitioned_vision['left'] =    float("{0:.6f}".format(str(sum(self.formatted_msg['ranges'][partitions[1]:partitions[2]])/len(self.formatted_msg['ranges'][partitions[1]:partitions[2]]))))
 
         return partitioned_vision
