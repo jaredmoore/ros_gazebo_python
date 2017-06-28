bba8a231292cdbad36c27622a6e558dfb74bb56a
Jared Moore <swiftfoottim@gmail.com>
2017-06-28 14:03:21 -0400

    Added print statements to test the different components of an individual.

--- src/basicbot_ga/test/ga_server.py
+++ src/basicbot_ga/test/ga_server.py
@@ -143,7 +143,7 @@ pop = toolbox.population(n=args.pop_size)
 for p in pop:
     print(p[0],p[1],p[2],p[3])
 
-    p.fitness = random.random()
+    p.fitness.values = (random.random(),)
     print(p.fitness)
 
 # # Run the first set of evaluations.
